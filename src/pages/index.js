38ea06d13cedc1c320e6a62b39e5a856a4e81e9d
Andrew Lee <andyrewlee@gmail.com>
2019-09-08 18:38:36 -0700

    Add copy to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import PageTemplate from '../templates/PageTemplate';
 
 const Home = () => (
   <PageTemplate>
-    <h1>Home</h1>
+    <h1>Core Standards</h1>
+    <h2>Kickstart your tech career</h2>
+    <p>With step by step lessons that focus on core programming concepts, Core Standards is the first step to your career in tech.</p>
   </PageTemplate>
 );
 
--- src/templates/PageTemplate.js
+++ src/templates/PageTemplate.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 
+const StyledDiv = styled.div`
+  max-width: 480px;
+`;
+
 const PageTemplate = ({ children }) => (
-  <>
+  <StyledDiv>
     <Navbar />
     {children}
-  </>
+  </StyledDiv>
 );
 
 export default PageTemplate;
